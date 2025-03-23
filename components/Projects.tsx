91aa7fffff4a95cbc06e1385ac417808e57264c3
Anamika Mukherjee <anamikamukherjee.raipur@gmail.com>
2025-03-23 17:50:41 +0530

    Style change

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () =>{
         <>
           <div className="projects-page" id="projects-section">
                 <div className="projects-container">
-                    <h1 className="heading text-[var(--default)] select-none">My Projects</h1>
+                    <h1 className="heading text-violet-300 select-none">My Projects</h1>
                   
                     <div className="w-full h-auto lg:h-full flex flex-col justify-center items-center space-y-10 p-10 lg:p-0">
                         <div className="w-full h-auto lg:h-1/2 flex lg:flex-row flex-col justify-between lg:justify-center items-center"> 
